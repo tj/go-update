68077c41e333e908cb7fd49e2233f5b48bfbfede
Dustin Deus <deusdustin@gmail.com>
2018-01-20 20:33:54 +0100

    use atomic fileutils copyFile method

--- update.go
+++ update.go
@@ -14,6 +14,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/pkg/fileutils"
+
 	"github.com/apex/log"
 	"github.com/c4milo/unpackit"
 	"github.com/pkg/errors"
@@ -81,31 +83,11 @@ func (m *Manager) InstallTo(path, dir string) error {
 
 	log.Debugf("copy %q to %q", bin, dst)
 
-	if err := copy(bin, dst); err != nil {
+	if err := fileutils.CopyFile(bin, dst); err != nil {
 		return errors.Wrap(err, "copy")
 	}
 
 	return nil
-}
-
-func copy(src, dst string) error {
-	in, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer in.Close()
-
-	out, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, in)
-	if err != nil {
-		return err
-	}
-	return out.Close()
 }
 
 // Install binary to replace the current version.
