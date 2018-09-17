44bf37de36a65ea2d86c8e714c33e5f250692a7e
TJ Holowaychuk <tj@vision-media.ca>
2018-09-17 13:36:41 -0700

    replace rename() with copy

--- update.go
+++ update.go
@@ -80,7 +80,7 @@ func (m *Manager) InstallTo(path, dir string) error {
 	dst := filepath.Join(dir, m.Command)
 
 	log.Debugf("move %q to %q", bin, dst)
-	if err := os.Rename(bin, dst); err != nil {
+	if err := copyFile(dst, bin); err != nil {
 		return errors.Wrap(err, "moving")
 	}
 
@@ -156,4 +156,50 @@ func (a *Asset) DownloadProxy(proxy Proxy) (string, error) {
 
 	log.Debugf("copied")
 	return f.Name(), nil
+}
+
+// copyFile copies the contents of the file named src to the file named
+// by dst. The file will be created if it does not already exist. If the
+// destination file exists, all it's contents will be replaced by the contents
+// of the source file. The file mode will be copied from the source and
+// the copied data is synced/flushed to stable storage.
+func copyFile(dst, src string) (err error) {
+	in, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return
+	}
+
+	defer func() {
+		if e := out.Close(); e != nil {
+			err = e
+		}
+	}()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return
+	}
+
+	err = out.Sync()
+	if err != nil {
+		return
+	}
+
+	si, err := os.Stat(src)
+	if err != nil {
+		return
+	}
+
+	err = os.Chmod(dst, si.Mode())
+	if err != nil {
+		return
+	}
+
+	return
 }
